a9a5cfe7202c4efdfd08cf28849b43bf8d2748fc
NicolasMC <nicolasmarcar2006@gmail.com>
2024-08-05 07:10:08 -0500

    feat:creacion de la clase VeterinaryClinic con sus propiedades, Constructores y cascaron de los metodos

--- Models/VeterinaryClinic.cs
+++ Models/VeterinaryClinic.cs
@@ -7,5 +7,69 @@ namespace PruebaC_sharp_NicolasMartinezCardona.Models;
 
 public class VeterinaryClinic
 {
+    public string Name { get; set; }
+
+    public string Address { get; set; }
+
+    public List<Dog> Dogs { get; set; }
+
+    public List<Cat> Cats { get; set; } 
+
+    public VeterinaryClinic()
+    {
+        List<Dog> Dogs = new List<Dog>();
+        List<Cat> Cats = new List<Cat>();
+    }
+
+    public VeterinaryClinic(string name, string address)
+    {
+        Name = name;
+        Address = address;
+    }
+
+    public void SaveDog(Dog newDog)
+    {
+
+    }
+
+    public void SaveCat(Cat newCat)
+    {
+        
+    }
+
+    public void UpdateDog(Dog dog)
+    {
+
+    }
+
+    public void UpdateCat(Cat cat)
+    {
+        
+    }
+
+    public void DeleteDog(int id)
+    {
+
+    }
+
+    public void DeleteCat(int id)
+    {
+
+    }
+
+    public void ShowAllPatients()
+    {
+
+    }
+
+    public void ShowAnimals(string type)
+    {
+
+    }
+
+    public void ShowPatients(int idPatient)
+    {
+
+    }
 
 }
