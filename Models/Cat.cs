4b38712b7f8f53036b4df38013756f95da5a40f6
NicolasMC <nicolasmarcar2006@gmail.com>
2024-08-05 06:56:15 -0500

    feat:creacion de la clase Cat con sus propiedades, Constructor y cascaron de los metodos

--- Models/Cat.cs
+++ Models/Cat.cs
@@ -5,9 +5,25 @@ using System.Threading.Tasks;
 
 namespace PruebaC_sharp_NicolasMartinezCardona.Models;
 
-public class Cat
+public class Cat : Animal
 {
+    public bool BreedingStatus { get; set; }
 
-    
+    public string FurLength { get; set; } 
 
+    public Cat(int id, string name, DateOnly birthdate, string breed, string color, double weightInKg, bool breedingStatus, string furLength) : base( id,  name,  birthdate,  breed,  color,  weightInKg)
+    {
+        BreedingStatus = breedingStatus;
+        FurLength = furLength;
+    }
+
+    public void CastrateAnimal()
+    {
+
+    }
+
+    public void Hairdress()
+    {
+        
+    }
 }
